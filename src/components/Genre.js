3b6da7b360b5cca642f61db2f57bc77862184d0f
Abhi Dadhaniya <abhidadhaniya23@gmail.com>
2022-02-04 15:20:57 +0530

    tried to fetch with axios but still not working

--- src/components/Genre.js
+++ src/components/Genre.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import MovieCard from "./MovieCard";
+import MovieItem from "./MovieItem";
+import axios from "axios";
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 
@@ -10,22 +11,36 @@ const Genre = () => {
   const [movie, setMovie] = useState([]);
   const { id } = useParams();
 
-  useEffect(async () => {
+  const fetchData = async () => {
     setGenreID(id);
-    setLoading(true);
+    console.log("1");
     const movieGenreApi = `https://api.themoviedb.org/3/discover/movie?with_genres=${genreID}&api_key=${API_KEY}&page=1`;
-    try {
-      fetch(movieGenreApi)
-        .then((response) => response.json())
-        .then((json) => setMovie(json.results))
-        .catch((err) => console.log(err));
-      console.log(movie);
-    } catch (err) {
-      console.log(err);
-    }
-    setTimeout(() => {
-      setLoading(false);
-    }, 950);
+    console.log("2");
+    const { data } = await axios.get(movieGenreApi);
+    console.log("3");
+    console.log(data.results);
+    setMovie(data.results);
+    console.log(movie);
+    console.log("4");
+  };
+
+  useEffect(() => {
+    console.log("5");
+    setLoading(true);
+    fetchData();
+    console.log("6");
+    // try {
+    //   fetch(movieGenreApi)
+    //     .then((response) => response.json())
+    //     .then((json) => setMovie(json.results))
+    //     .catch((err) => console.log(err));
+    //   console.log(movie);
+    // } catch (err) {
+    //   console.log(err);
+    // }
+    // setTimeout(() => {
+    setLoading(false);
+    // }, 950);
   }, [id]);
 
   return (
@@ -35,7 +50,7 @@ const Genre = () => {
       ) : (
         <div className="container flex flex-row flex-wrap items-center justify-center mx-auto">
           {movie.map((item, index) => (
-            <MovieCard key={index} movieItem={item} />
+            <MovieItem key={index} movieItem={item} />
           ))}
         </div>
       )}
