3afadf1edf958fb8757648afa5a8f1625e340cf1
Abhi Dadhaniya <abhidadhaniya23@gmail.com>
2022-02-07 13:15:19 +0530

    update build

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,9 +3,7 @@ import { Link, useLocation } from "react-router-dom";
 
 const Nav = () => {
   let location = useLocation();
-  useEffect(() => {
-    console.log(location);
-  }, [location]);
+  useEffect(() => {}, [location]);
   return (
     <div className="fixed bottom-0 z-10 flex flex-row items-center w-full py-3 bg-gray-900 md:py-5 justify-evenly md:justify-center text-light">
       <Link className={`text-center px-4 text-base duration-300 hover:scale-110 md:text-xl ${location.pathname === "/" ? "opacity-100" : "opacity-60"}`} to="/">
