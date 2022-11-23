c475c45fa11819ef11e2f6ca4d00b8b236a09983
Abhi Dadhaniya <abhidadhaniya23@gmail.com>
2022-11-23 08:06:57 +0530

    update portfolio link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,20 @@
 import React from "react";
 
 const Footer = () => {
-    return (
-        <div className="py-5 -mt-10 -translate-y-16 md:translate-y-0 text-xl md:text-2xl text-center text-gray-100 bg-dark">
-            Copyright&nbsp;&copy; ||&nbsp; Created&nbsp; with&nbsp; <i className="fas fa-heart"></i>&nbsp; By&nbsp;
-            <a rel="noreferrer" href="https://www.web-developer-abhi.me/" target="_blank" className="text-light">
-                Abhi
-            </a>
-        </div>
-    );
+  return (
+    <div className="py-5 -mt-10 -translate-y-16 md:translate-y-0 text-xl md:text-2xl text-center text-gray-100 bg-dark">
+      Copyright&nbsp;&copy; ||&nbsp; Created&nbsp; with&nbsp;{" "}
+      <i className="fas fa-heart"></i>&nbsp; By&nbsp;
+      <a
+        rel="noreferrer"
+        href="https://www.abhidadhaniya.com/"
+        target="_blank"
+        className="text-light"
+      >
+        Abhi
+      </a>
+    </div>
+  );
 };
 
 export default Footer;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,37 +2,62 @@ import { useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 
 const Nav = () => {
-    let location = useLocation();
-    useEffect(() => {}, [location]);
-    return (
-        <div className="fixed bottom-0 z-10 flex flex-row items-center w-full py-3 bg-gray-900 h-min md:top-0 md:py-5 justify-evenly md:justify-center text-light">
-            <Link className={`text-center px-1 md:px-4 text-base mx-0.5 md:mx-2 duration-100 hover:opacity-100 active:scale-90 md:text-xl ${location.pathname === "/" ? "opacity-100" : "opacity-40"}`} to="/">
-                <i className="md:mr-3 fab fa-typo3"></i>
-                <br className="md:hidden" />
-                Genres
-            </Link>
-            <Link className={`text-center px-1 md:px-4 text-base mx-0.5 md:mx-2 duration-100 hover:opacity-100 active:scale-90 md:text-xl ${location.pathname === "/Trending" ? "opacity-100" : "opacity-40"}`} to="/Trending">
-                <i className="md:mr-3 fas fa-fire"></i>
-                <br className="md:hidden" />
-                Trending
-            </Link>
-            <Link className={`text-center px-1 md:px-4 text-base mx-0.5 md:mx-2 duration-100 hover:opacity-100 active:scale-90 md:text-xl ${location.pathname === "/Search-Movie" ? "opacity-100" : "opacity-40"}`} to="/Search-Movie">
-                <i className="md:mr-3 fas fa-search"></i>
-                <br className="md:hidden" />
-                Search
-            </Link>
-            <a className="px-1 md:px-4 mx-0.5 text-base text-center duration-100 md:mx-2 active:scale-90 md:text-xl opacity-40 hover:opacity-100" rel="noreferrer" href="https://www.web-developer-abhi.me/" target="_blank">
-                <i className="md:mr-3 fas fa-code"></i>
-                <br className="md:hidden" />
-                Developer
-            </a>
-            <a className="px-1 md:px-4 mx-0.5 text-base text-center duration-100 md:mx-2 active:scale-90 md:text-xl opacity-40 hover:opacity-100" rel="noreferrer" href="https://twitter.com/AbhiDadhaniya3" target="_blank">
-                <i className="md:mr-3 fab fa-twitter"></i>
-                <br className="md:hidden" />
-                Follow
-            </a>
-        </div>
-    );
+  let location = useLocation();
+  useEffect(() => {}, [location]);
+  return (
+    <div className="fixed bottom-0 z-10 flex flex-row items-center w-full py-3 bg-gray-900 h-min md:top-0 md:py-5 justify-evenly md:justify-center text-light">
+      <Link
+        className={`text-center px-1 md:px-4 text-base mx-0.5 md:mx-2 duration-100 hover:opacity-100 active:scale-90 md:text-xl ${
+          location.pathname === "/" ? "opacity-100" : "opacity-40"
+        }`}
+        to="/"
+      >
+        <i className="md:mr-3 fab fa-typo3"></i>
+        <br className="md:hidden" />
+        Genres
+      </Link>
+      <Link
+        className={`text-center px-1 md:px-4 text-base mx-0.5 md:mx-2 duration-100 hover:opacity-100 active:scale-90 md:text-xl ${
+          location.pathname === "/Trending" ? "opacity-100" : "opacity-40"
+        }`}
+        to="/Trending"
+      >
+        <i className="md:mr-3 fas fa-fire"></i>
+        <br className="md:hidden" />
+        Trending
+      </Link>
+      <Link
+        className={`text-center px-1 md:px-4 text-base mx-0.5 md:mx-2 duration-100 hover:opacity-100 active:scale-90 md:text-xl ${
+          location.pathname === "/Search-Movie" ? "opacity-100" : "opacity-40"
+        }`}
+        to="/Search-Movie"
+      >
+        <i className="md:mr-3 fas fa-search"></i>
+        <br className="md:hidden" />
+        Search
+      </Link>
+      <a
+        className="px-1 md:px-4 mx-0.5 text-base text-center duration-100 md:mx-2 active:scale-90 md:text-xl opacity-40 hover:opacity-100"
+        rel="noreferrer"
+        href="https://www.abhidadhaniya.com/"
+        target="_blank"
+      >
+        <i className="md:mr-3 fas fa-code"></i>
+        <br className="md:hidden" />
+        Developer
+      </a>
+      <a
+        className="px-1 md:px-4 mx-0.5 text-base text-center duration-100 md:mx-2 active:scale-90 md:text-xl opacity-40 hover:opacity-100"
+        rel="noreferrer"
+        href="https://twitter.com/AbhiDadhaniya3"
+        target="_blank"
+      >
+        <i className="md:mr-3 fab fa-twitter"></i>
+        <br className="md:hidden" />
+        Follow
+      </a>
+    </div>
+  );
 };
 
 export default Nav;
